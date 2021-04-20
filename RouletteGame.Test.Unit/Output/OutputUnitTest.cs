d12143c9c8ca3e9563847c89bf29707f71c09467
Frank Bodholdt Jakobsen <au237297@uni.au.dk>
2021-04-20 21:51:19 +0200

    Add project files.

---
+++ RouletteGame.Test.Unit/Output/OutputUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using NUnit.Framework;
+using RouletteGame.Output;
+
+namespace RouletteGame.Test.Unit.Output
+{
+    [TestFixture]
+    public class OutputUnitTest
+    {
+        private StringWriter fakeConsole;
+        private ConsoleOutput uut;
+        [SetUp]
+        public void Setup()
+        {
+            fakeConsole = new StringWriter();
+            System.Console.SetOut(fakeConsole);
+            uut = new ConsoleOutput();
+        }
+
+        [Test]
+        public void Report_Text_CorrectOutput()
+        {
+            uut.Report("Test string");
+            Assert.That(fakeConsole.ToString().Contains("Test string"));
+        }
+        
+    }
+}
